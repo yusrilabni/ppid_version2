27ba86d0318353980dde0f31ba4ef12a3a40183a
Yusril Abni <yusrilabni@example.com>
2026-03-30 10:44:33 +0800

    Simplified path for root ppid_version2 in cPanel

--- public/index.php
+++ public/index.php
@@ -4,15 +4,15 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// 1. Cek Maintenance Mode
+// 1. Cek Maintenance Mode (Arahkan langsung ke ppid_version2)
 if (file_exists($maintenance = __DIR__.'/../../ppid_version2/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// 2. Register Autoloader (Arahkan ke ppid_version2)
+// 2. Register Autoloader
 require __DIR__.'/../../ppid_version2/vendor/autoload.php';
 
-// 3. Bootstrap Laravel (Arahkan ke ppid_version2)
+// 3. Bootstrap Laravel
 $app = require_once __DIR__.'/../../ppid_version2/bootstrap/app.php';
 
 // 4. Handle Request
