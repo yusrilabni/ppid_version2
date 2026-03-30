5cfce2dc6501190657fb9931cc8fb92d3a388a39
Yusril Abni <yusrilabni@example.com>
2026-03-30 12:25:12 +0800

    Enable error reporting in index.php for debugging

--- public/index.php
+++ public/index.php
@@ -1,17 +1,17 @@
 <?php
 
+// Paksa tampilkan error untuk debugging
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Path absolut yang sudah terbukti jalan di tes tadi
 $basePath = '/home/ppidkab/ppid_version2';
 
-// 1. Register Autoloader
 require $basePath . '/vendor/autoload.php';
-
-// 2. Bootstrap Laravel
 $app = require_once $basePath . '/bootstrap/app.php';
 
-// 3. Handle Request
 $app->handleRequest(Request::capture());
