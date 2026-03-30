ebb1fe332fc27d9c5642ec7aba85227e8f49ce80
Yusril Abni <yusrilabni@example.com>
2026-03-30 12:01:25 +0800

    Pull-ready files for v2 folder

--- public/index.php
+++ public/index.php
@@ -1,13 +1,10 @@
 <?php
-// Diagnostik Error
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
 
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Arahkan ke ppid_version2 di root home
+// Arahkan ke ppid_version2 di root home cPanel
 require __DIR__.'/../../ppid_version2/vendor/autoload.php';
 $app = require_once __DIR__.'/../../ppid_version2/bootstrap/app.php';
 
