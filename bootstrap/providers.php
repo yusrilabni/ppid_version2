7645f61c13fbff6bc0734a62a811bab3102b7a9a
Yusril Abni <yusrilabni@example.com>
2026-05-11 16:59:15 +0800

    Register LaravelERD Provider for recca0120 package

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,4 +4,5 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\ViewServiceProvider::class,
     Intervention\Image\ImageServiceProvider::class,
+    Recca0120\LaravelErd\LaravelErdServiceProvider::class,
 ];
