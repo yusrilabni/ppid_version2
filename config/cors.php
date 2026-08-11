74a2c29c22b99f0edb357dc8e7641c9e53ad643d
Yusril Abni <yusrilabni@example.com>
2026-08-11 14:35:28 +0800

    Allow all CORS origins

--- config/cors.php
+++ config/cors.php
@@ -19,12 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        env('FRONTEND_URL', 'http://localhost:3000'),
-        'http://localhost:3000',
-        'http://127.0.0.1:3000',
-        'https://*.vercel.app',
-    ],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [
         '#^https://.*\.vercel\.app$#',
