a03d238cb42f389cdc40566117830ec08a84caa9
Yusril Abni <yusrilabni@example.com>
2026-09-15 10:36:05 +0800

    chore: test cron at 10:37

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,8 +26,8 @@ class Kernel extends ConsoleKernel
     {
         $schedule->command('ppid:update-informasi-category')->daily();
         
-        // Backup database setiap jam 12 malam (00:00) - Uji coba setiap menit
-        $schedule->command('ppid:backup-db')->everyMinute();
+        // Backup database setiap jam 12 malam (00:00) - Uji coba 10:37
+        $schedule->command('ppid:backup-db')->dailyAt('10:37');
     }
 
     /**
